f255467f51835f26d639e3be0f2b5ef25014eb23
Matheus Diógenes <mathdiogenes@gmail.com>
2023-12-22 15:35:25 -0300

    feat: Fertilizer model

---
+++ src/main/java/com/betrybe/agrix/models/entities/Fertilizer.java
@@ -0,0 +1,64 @@
+package com.betrybe.agrix.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * The type Fertilizer.
+ */
+@Entity
+@Table(name = "fertilizers")
+public class Fertilizer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String brand;
+  private String composition;
+
+  public Fertilizer() {
+  }
+
+  public Fertilizer(Long id, String name, String brand, String composition) {
+    this.id = id;
+    this.name = name;
+    this.brand = brand;
+    this.composition = composition;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getComposition() {
+    return composition;
+  }
+
+  public void setComposition(String composition) {
+    this.composition = composition;
+  }
+}
